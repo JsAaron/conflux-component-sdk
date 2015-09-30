0235af00590d1f35830166f648e32bcac2920775
jsAaron <401545553@qq.com>
2015-09-30 22:55:10 +0800

    left

--- cardGame/js/app.js
+++ cardGame/js/app.js
@@ -1,161 +1,181 @@
 
-function format(format) {
-    var args = [].slice.call(arguments, 1, arguments.length);
-    return format.replace(/\{(\d+)\}/g, function(m, i) {
-        return args[i];
-    });
-}
-
-
 /**
  * 翻牌小游戏
  * @type {Object}
  */
-var cardGames = function(element) {
+;(function() {
 
-    //页面容器
-    var $container = $(element);
-    this.$container = $container;
+    //配置文件
+    var config = {
 
-    //区域尺寸
-    this.contentWidth = parseInt($container.css('width'))
-    this.contentHeight = parseInt($container.css('Height'))
+        //图片
+        images: {
+            //正面图
+            front: "images/1.png",
+            //背景图,随机分配
+            back: ["images/11.png", "images/12.png", "images/13.png"]
+        },
 
-    //区域布局
-    var offset = $container.offset()
-    this.contentLeft = offset.left;
-    this.contentTop = offset.top
+        //翻转的速度单位ms //默认0.6秒
+        speed:600,
 
-    this.images = {
-        front : "images/1.png",
-        back  : "images/3.jpg"
+        //反转的反向
+        //顺时针//逆时针 left or rigth
+        direction:'left'
+
     }
 
-    this.initCreate();
-    this.creatEvent();
-};
-
-
-cardGames.prototype = {
-
-
-    initCreate: function() {
-        this.level = {
-            row: 3, //横行
-            col: 2 //竖列
-        }
-        this.debrisWidth = this.contentWidth / this.level.row;
-        this.debrisHeight = this.contentHeight / this.level.col;
-        //初始化布局
-        this.initLayer();
-    },
-
-    initLayer: function() {
-        var index;
-        var $ul;
-        var $li;
-        var uls          = [];
-        var debrisWidth  = this.debrisWidth;
-        var debrisHeight = this.debrisHeight;
-        var row          = this.level.row;
-        var col          = this.level.col;
-        var images       = this.images;
-
-        //布局的原始排序
-        this.originalOrder = {};
-
-        //碎片快速索引
-        this.$debrisMap = {};
-
-        var uls = [];
-
-        var createStr = function(i,j) {
-            var fill = function() {
-                return 'width:100%;height:100%;position:absolute;top:0;left:0;background-size:100% 100%;backface-visibility: hidden;'
-            }
-            var innerdiv = function() {
-                return format(
-                    '<div style="{0};left;z-index:2;background-image:{1}"></div>'+
-                    '<div style="{0};transform: rotateY(180deg);-webkit-transform:rotateY(180deg);background-image:{2};"></div>',
-                    fill(), "url('" + images.front + "')", "url('" + images.back + "')"
-                )
-            }
-            var str = format(
-                '<li data-page = {0} data-index ={1} '+
-                    'style="width:{2}px;height:{3}px;left:{4}px;top:{5}px;'+
-                    'background-size:100% 100%;'+
-                    'position:absolute;' +
-                    'transition: 0.6s;' +
-                    'transform-style: preserve-3d;' +
-                    'transform:scale(0.9);' +
-                    '-webkit-transition: 0.6s;' +
-                    '-webkit-transform-style: preserve-3d;' +
-                    '-webkit-transform:scale(0.9);"' +
-                '>'+ innerdiv() +'</li>', 
-                i, j,
-                debrisWidth, debrisHeight, j * debrisWidth, i * debrisHeight
-            )
-            return $(str)
-        }
-
-        //生成 row * col 的矩阵
-        for (var i = 0; i < col; i++) {
-            $ul = $(document.createElement('ul')).css({
-                'width': this.contentWidth,
-                'height': this.contentHeight / 2
-            });
-            for (var j = 0; j < row; j++) {
-                $li = createStr(i,j);
-                $ul.append($li)
-                if (!this.originalOrder[i]) {
-                    this.originalOrder[i] = [];
-                }
-                this.originalOrder[i].push(j)
-            }
-            uls.push($ul)
-        }
-
-        var $fragment = $(document.createElement('createDocumentFragment'));
-        $.each(uls, function(index, ul) {
-            $fragment.append(ul)
-        })
-        this.$container.append($fragment[0].childNodes);
-    },
-
-
-    onClick: function(event) {
-        var element, elementName, page;
-        element = event.target;
-        if (element && (element = element.parentNode)) {
-            elementName = element.nodeName.toLowerCase();
-            if (elementName == 'li') {
-                page = element.getAttribute('data-page');
-                if (page || page == 0) {
-                    $(element).css({
-                        'transform': 'rotateY(180deg),scale(0.9)'
-                    })
-                }
-            }
-        }
-    },
-
-    transitionend: function() {
-
-    },
-
-    creatEvent: function() {
-        var stopBehavior = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        var self = this;
-        this.$container.on('mousedown touchstart', function(event) {
-            stopBehavior(event)
-            self.onClick(event)
-        }).on('transitionend webkitTransitionEnd MSTransitionEnd oTransitionEnd', function(event) {
-            self.transitionend(event)
+    function format(format) {
+        var args = [].slice.call(arguments, 1, arguments.length);
+        return format.replace(/\{(\d+)\}/g, function(m, i) {
+            return args[i];
         });
     }
 
 
-}
+    config.rotateY = config.direction === 'left' ? '180deg' : '-180deg';
+
+
+    var cardGames = function(element, options) {
+
+        //页面容器
+        var $container = $(element);
+        this.$container = $container;
+
+        //区域尺寸
+        this.contentWidth = parseInt($container.css('width'))
+        this.contentHeight = parseInt($container.css('Height'))
+
+        //区域布局
+        var offset = $container.offset()
+        this.contentLeft = offset.left;
+        this.contentTop = offset.top
+
+        //触发翻转动画
+        this.trigger = [];
+
+        this.initCreate();
+        this.creatEvent();
+    };
+
+
+    cardGames.prototype = {
+
+        initCreate: function() {
+            this.level = {
+                row: 3, //横行
+                col: 2 //竖列
+            }
+            this.debrisWidth = this.contentWidth / this.level.row;
+            this.debrisHeight = this.contentHeight / this.level.col;
+            //初始化布局
+            this.initLayer();
+        },
+
+        initLayer: function() {
+            var index;
+            var $ul;
+            var $li;
+            var uls = [];
+            var debrisWidth = this.debrisWidth;
+            var debrisHeight = this.debrisHeight;
+            var row = this.level.row;
+            var col = this.level.col;
+            var images = config.images;
+
+            //布局的原始排序
+            this.originalOrder = {};
+
+            //碎片快速索引
+            this.$debrisMap = {};
+
+            var uls = [];
+
+            var createStr = function(i, j) {
+                var fill = function() {
+                    return 'width:100%;height:100%;position:absolute;top:0;left:0;background-size:100% 100%;backface-visibility: hidden;'
+                }
+                var innerdiv = function() {
+                    return format(
+                        '<div style="{0};left;z-index:2;background-image:{1}"></div>' +
+                        '<div style="{0};transform: rotateY({2});-webkit-transform:rotateY({2});background-image:{3};"></div>',
+                        fill(), "url('" + images.front + "')", config.rotateY, "url('" + images.back[j] + "')"
+                    )
+                }
+                var str = format(
+                    '<li data-page = {0} data-index ={1} ' +
+                    'style="width:{2}px;height:{3}px;left:{4}px;top:{5}px;' +
+                    'background-size:100% 100%;' +
+                    'position:absolute;' +
+                    'transform-style: preserve-3d;' +
+                    'transform:scale(0.9);' +
+                    '-webkit-transform-style: preserve-3d;' +
+                    '-webkit-transform:scale(0.9);"' +
+                    '>' + innerdiv() + '</li>',
+                    i, j,
+                    debrisWidth, debrisHeight, j * debrisWidth, i * debrisHeight
+                )
+                return $(str)
+            }
+
+            //生成 row * col 的矩阵
+            for (var i = 0; i < col; i++) {
+                $ul = $(document.createElement('ul')).css({
+                    'width': this.contentWidth,
+                    'height': this.contentHeight / 2
+                });
+                for (var j = 0; j < row; j++) {
+                    $li = createStr(i, j);
+                    $ul.append($li)
+                    if (!this.originalOrder[i]) {
+                        this.originalOrder[i] = [];
+                    }
+                    this.originalOrder[i].push(j)
+                }
+                uls.push($ul)
+            }
+
+            var $fragment = $(document.createElement('createDocumentFragment'));
+            $.each(uls, function(index, ul) {
+                $fragment.append(ul)
+            })
+            this.$container.append($fragment[0].childNodes);
+        },
+
+        onClick: function(event) {
+            var element, elementName, page;
+            element = event.target;
+            if (element && (element = element.parentNode)) {
+                elementName = element.nodeName.toLowerCase();
+                if (elementName == 'li') {
+                    $(element).css({
+                        'transition-duration': config.speed + 'ms',
+                        'transform': 'rotateY(' + config.rotateY + '),scale(0.9)'
+                    })
+                    this.trigger.push(element);
+                }
+            }
+        },
+
+        transitionend: function(e) {
+           
+        },
+
+        creatEvent: function() {
+            var stopBehavior = function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            var self = this;
+            this.$container.on('mousedown touchstart', function(event) {
+                stopBehavior(event)
+                self.onClick(event)
+            }).on('transitionend webkitTransitionEnd MSTransitionEnd oTransitionEnd', function(event) {
+                self.transitionend(event)
+            });
+        }
+    }
+
+
+    window.cardGames = cardGames;
+})();
