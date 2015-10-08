99b4c97ab3866ada791d32f5a5f8e5e4e30bdf55
jsAaron <401545553@qq.com>
2015-10-08 19:58:01 +0800

    11

--- cardGame/release/app.js
+++ cardGame/release/app.js
@@ -1,8 +1,12 @@
 ;(function() {
-    var $homePage = $('.home-page');
-    var $contentPage = $('.content-page');
 
-    var $element = $('.banner-right .score')
+	//游戏时间
+	var GameTime = 30000; //ms单位 
+
+
+	var $homePage    = $('.home-page');
+	var $contentPage = $('.content-page');
+	var $element     = $('.banner-right .score');
 
     //目前分数
     var score = 0;
@@ -22,20 +26,69 @@
         update();
     }
 
-    $('.start-button').on('click', function(e) {
-        startContent(e)
-    })
-    // startContent()
+
+	var SlideBox = function() {
+		var startTime;
+		var timer;
+		var $dotWrap = $('.dot-wrap');
+		var $ems     = $dotWrap.find('em')
+		var vernier  = $ems.length;
+		var rate     = GameTime/vernier;
+
+		function updatedot() {
+			var em;
+			if (em = $ems[--vernier]) {
+				em.style.backgroundColor = '#291669';
+			}
+		}
+
+		function run() {
+			timer = setTimeout(function() {
+				updatedot();
+				run();
+			}, rate)
+		}
+
+		this.start = function() {
+			startTime = new Date().getTime();
+			run();
+		}
+
+		this.destroy = function() {
+			clearTimeout(timer)
+			vernier = $ems.length;
+			$ems.css('backgroundColor', '#d3aa4e')
+		}
+
+	}
+	var slidebox = new SlideBox();
 
 
-    var passTime = 0;
-    function createGames(className) {
+	//游戏次数
+    var GameTotal = 1;
+    //内容节点class名
+    var className = '.content-page-card';
+
+	//选择游戏
+	function selectGame() {
+		if (GameTotal >= 4) {
+			alert('游戏结束')
+			return;
+		}
+		createGames();
+	}
+
+
+    function createGames() {
+    	slidebox.start();
+    	++GameTotal;
         var cardGames = new CardGames(className)
         cardGames.$watch('success', add)
         cardGames.$watch('fail', reduce)
         cardGames.$watch('complete', function() {
-            cardGames.destroy();
-            createGames(className);
+			slidebox.destroy();
+			cardGames.destroy();
+			selectGame()
         })
     }
 
@@ -46,4 +99,9 @@
         $homePage.hide()
     }
 
+
+    $('.start-button').on('click', function(e) {
+        startContent(e)
+    })
+  	// startContent()
 })()
