e70da1cc5c10064805297cf8803ead53512f81d5
JsAaron <401545553@qq.com>
2015-11-20 22:48:32 +0800

    update

--- christmas/release/js/pageB.js
+++ christmas/release/js/pageB.js
@@ -1,18 +1,19 @@
 /**
- * ç¬¬äºŒå‰¯åœºæ™¯é¡µé¢
+ * µÚ¶ş¸±³¡¾°Ò³Ãæ
  *
  */
 function PageB(element) {
 
     var $element  = element;
-    //åœ£è¯ç”·å­©
+    //Ê¥µ®ÄĞº¢
     var $boy      = $element.find(".boy-walk");
-    //3dæ—‹è½¬
+    //3dĞı×ª
     var $carousel = $element.find("#carousel");
-    //å¥³å­©
+    //Å®º¢
     var $girl     = $element.find(".girl-book");
-    //çŒ«
+    //Ã¨
     var $cat      = $element.find(".cat");
+
 
     $boy.transition({
         "right": "5rem"
@@ -21,25 +22,47 @@ function PageB(element) {
         setTimeout(function() {
             $girl.addClass("girl-book-getUp");
             setTimeout(function() {
-                // $girl.addClass("girl-book-run")
+                $girl.addClass("girl-book-run")
+                //Ã¨Êé
                 $cat.addClass("cat-book");
-                $girl.addClass("girl-run")
-            }, 500)
+                //Ğ¡Å®º¢×ßÂ·
+                $girl.addClass("girl-walk")
+                    .transition({
+                        "left": "5.5rem"
+                    }, 500, "linear", function() {
+                        $girl.addClass("walk-stop")
+                        // $girl.addClass("girl-choose").addClass("walk-run")
+                    })
+
+
+
+            }, 1000)
         }, 500)
-        $boy.addClass("boy-take-gift")
-        $boy.one(support.animationEnd,function(){
-            showCarousel();
+
+
+        //´ò¿ª°ü¹ü
+        $boy
+        .addClass("walk-stop")
+        .addClass("boy-unwrapp")
+        .addClass("walk-run")
+        .one(support.animationEnd, function() {
+            showCarousel()
+
+            setTimeout(function(){
+                 $girl.addClass("girl-choose").addClass("walk-run")
+            },1000)
+
         })
     });
 
-   
+
 
     /**
-     * æ˜¾ç¤º3dæ—‹è½¬æœ¨é©¬
+     * ÏÔÊ¾3dĞı×ªÄ¾Âí
      * @return {[type]} [description]
      */
     function showCarousel(){
-         //3dæ—‹è½¬
+         //3dĞı×ª
         var carousel = new Carousel($carousel, {
             imgUrls: [
                 "assets/carousel/1.png",
@@ -56,3 +79,33 @@ function PageB(element) {
     }
 
 }
+
+
+    /**
+     *  css3¹Ø¼üÖ¡Ëã·¨
+     * @param {[type]} row   [description]
+     * @param {[type]} col   [description]
+     * @param {[type]} count [description]
+     */
+    function calculationKeyframes(col, row, count) {
+        //¾ØÕóÉú³ÉstepµÄ´¦Àí
+        //  0 1 2
+        //  3 4 5
+        //  6 7 8
+        var keyframes = [];
+        var base = 100 / count;
+        //Ê×´Î
+        keyframes.push(0 + '% { background-position:0% 0%}')
+        for (var i = 0; i < count; i++) {
+            //µ±Ç°ĞĞÊı
+            var currRow = Math.ceil((i + 1) / col); //µ±Ç°ĞĞÊı
+            var currCol = Math.floor(i / col); //µ±Ç°ÁĞÊı  
+            var period = currCol * col; //Ã¿¶ÎÊıÁ¿  
+            var x = 100 * (i - period)
+            var y = 100 * currCol;
+            x = x == 0 ? x : "-" + x;
+            y = y == 0 ? y : "-" + y;
+            keyframes.push(((i + 1) * base) + '% { background-position: ' + x + '% ' + y + '%}')
+        }
+        return keyframes.join("")
+    }
