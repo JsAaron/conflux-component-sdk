fbcd97dbda20488dab18fd5551311ecddddfe443
jsAaron <401545553@qq.com>
2015-11-20 16:50:28 +0800

    update

--- christmas/release/js/pageB.js
+++ christmas/release/js/pageB.js
@@ -4,21 +4,35 @@
  */
 function PageB(element) {
 
-    var $root = element;
+    var $element  = element;
     //圣诞男孩
-    var $boy = $root.find(".boy-walk");
+    var $boy      = $element.find(".boy-walk");
     //3d旋转
-    var $carousel = $root.find("#carousel");
-
-
+    var $carousel = $element.find("#carousel");
+    //女孩
+    var $girl     = $element.find(".girl-book");
+    //猫
+    var $cat      = $element.find(".cat");
 
     $boy.transition({
         "right": "5rem"
-    }, 3000, "linear", function() {
-        $boy.addClass("boy-walk-stop").removeClass("boy-walk-animate")
+    }, 500, "linear", function() {
+        $boy.removeClass("boy-walk-animate")
+        setTimeout(function() {
+            $girl.addClass("girl-book-getUp");
+            setTimeout(function() {
+                // $girl.addClass("girl-book-run")
+                $cat.addClass("cat-book");
+                $girl.addClass("girl-run")
+            }, 500)
+        }, 500)
+        $boy.addClass("boy-take-gift")
+        $boy.one(support.animationEnd,function(){
+            showCarousel();
+        })
     });
 
-    showCarousel();
+   
 
     /**
      * 显示3d旋转木马
