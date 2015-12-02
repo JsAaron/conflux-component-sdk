b4e4686478c0715b01f811d4539fa663810e5b1c
jsAaron <401545553@qq.com>
2015-12-02 15:05:36 +0800

    update

--- christmas/release/js/config.js
+++ christmas/release/js/config.js
@@ -4,32 +4,23 @@
 
 var config = {
 
-    //是否维持缩放比
-    keepZoomRatio: true,
+    //全屏
+    fullscreen: true,
 
     //设置容器尺寸
-    //否则默认全屏
     //如果设置，需要输入具体的px值
     layer: {
-		'width'  : 800,
-		'top'    : 0,
-		'left'   : 0
+        'width' : "800",
+        'top'   : 0,
+        'left'  : 0
     }
 }
 
-//根据页面尺寸维持缩放比
-if (config.keepZoomRatio) {
-    var proportion = 900/1440
+var proportion = 900 / 1440;
+if (config.fullscreen) {
+    var width = document.documentElement.clientWidth;
+    config.layer.width = width;
+    config.layer.height = width * proportion
+} else {
     config.layer.height = config.layer.width * proportion
-    // console.log(proportion)
-/*    // 原始比例
-    var proportionY = 900 / 1440
-    var screenHeight = config.layer.height;
-    //维持正比缩放的高度
-    var zooomHeight = screenHeight * proportionY;
-    var zooomTop = (screenHeight - zooomHeight) / 2
-    //设置正比缩放的数据
-	config.layer.height = zooomHeight;
-	config.layer.top    = zooomTop;*/
 }
-
