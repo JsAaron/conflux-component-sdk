7f53049746e46302a8f0e557eefd9d4c2e438cbe
jsAaron <401545553@qq.com>
2015-12-10 16:39:03 +0800

    udpate

--- christmas/release/js/pageB.js
+++ christmas/release/js/pageB.js
@@ -280,9 +280,9 @@ function PageB(element, pageComplete) {
         //3d旋转
         var carousel = new Carousel($carousel, {
             imgUrls: [
-                "http://img.mukewang.com/5662e29a0001905a14410901.png",
+                "http://img.mukewang.com/5662e26f00010dea14410901.png",
                 "http://img.mukewang.com/5662e2960001f16314410901.png",
-                "http://img.mukewang.com/5662e26f00010dea14410901.png"
+                "http://img.mukewang.com/5662e29a0001905a14410901.png"
             ],
             videoUrls: [
                 "http://www.imooc.com/upload/media/qx-one.mp4",
