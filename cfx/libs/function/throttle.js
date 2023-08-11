5c4100880dd138d001356e5a957c10bfa917db61
chenwen <401545553@qq.com>
2023-08-11 11:52:47 +0800

    update

--- cfx/libs/function/throttle.js
+++ cfx/libs/function/throttle.js
@@ -1,34 +1,30 @@
-let timeoutArr = [];
-let flagArr = [];
+let timer; let
+    flag
 /**
- * 节流函数
  * 节流原理：在一定时间内，只能触发一次
- * @param {Function} fn 要执行的回调函数 
- * @param {Number} time 延时的时间
- * @param {Boolean} isImmediate 是否立即执行
- * @param {String} timeoutName 定时器ID
+ *
+ * @param {Function} func 要执行的回调函数
+ * @param {Number} wait 延时的时间
+ * @param {Boolean} immediate 是否立即执行
  * @return null
  */
-function throttle(fn, time = 500, isImmediate = true, timeoutName = "default") {
-	if(!timeoutArr[timeoutName]) timeoutArr[timeoutName] = null;
-	if (isImmediate) {
-		if (!flagArr[timeoutName]) {
-			flagArr[timeoutName] = true;
-			// 如果是立即执行，则在time毫秒内开始时执行
-			if(typeof fn === 'function') fn();
-			timeoutArr[timeoutName] = setTimeout(() => {
-				flagArr[timeoutName] = false;
-			}, time);
-		}
-	} else {
-		if (!flagArr[timeoutName]) {
-			flagArr[timeoutName] = true;
-			// 如果是非立即执行，则在time毫秒内的结束处执行
-			timeoutArr[timeoutName] = setTimeout(() => {
-				flagArr[timeoutName] = false;
-				if(typeof fn === 'function') fn();
-			}, time);
-		}
-	}
-};
+function throttle(func, wait = 500, immediate = true) {
+    if (immediate) {
+        if (!flag) {
+            flag = true
+            // 如果是立即执行，则在wait毫秒内开始时执行
+            typeof func === 'function' && func()
+            timer = setTimeout(() => {
+                flag = false
+            }, wait)
+        }
+    } else if (!flag) {
+        flag = true
+        // 如果是非立即执行，则在wait毫秒内的结束处执行
+        timer = setTimeout(() => {
+            flag = false
+            typeof func === 'function' && func()
+        }, wait)
+    }
+}
 export default throttle
